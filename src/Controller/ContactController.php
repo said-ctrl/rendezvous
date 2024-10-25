e2a152a6114bde623ddcb037e6b7034ded007456
said-ctrl <fifinabs@gmail.com>
2024-10-25 11:51:37 +0200

    ede

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,21 +13,27 @@ use Symfony\Component\Routing\Attribute\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entitymanager): Response
     {
 
-        $form = $this->createForm(ContactType::class);
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
         
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
+            $entitymanager->persist($contact);
+            $entitymanager->flush();
+
+
             // Ici, vous pouvez gérer l'envoi de l'email
-            $data = $form->getData();
+            // $data = $form->getData();
             // Utiliser SwiftMailer ou une autre méthode pour envoyer l'email
             
             $this->addFlash('success', 'Votre message a été envoyé avec succès.');
 
-            return $this->redirectToRoute('meeting_index');
+            return $this->redirectToRoute('app_contact');
         }
 
         return $this->render('contact/index.html.twig', [
